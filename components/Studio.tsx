9d39055c7a6ee10ad61088e18348415d64633fce
Paul Han <paulhan@MacBook-Pro-250.local>
2022-12-13 15:21:00 -0800

    updated with OS notes

--- components/Studio.tsx
+++ components/Studio.tsx
@@ -6,17 +6,17 @@ export default function Studio() {
       <div className = "projects studio-section">
         <div className="project-title title">Projects</div>
         <ProjectLink title = "Real time paint" link="/projects/paint" date = "October 2022"></ProjectLink>
+        <ProjectLink title = "Social media app" link="/projects/social" date = "August 2022"></ProjectLink>
         <ProjectLink title = "Feline Frenzy" link="/projects/unity" date = "June 2022"></ProjectLink>
         <ProjectLink title = "HTTP web server" link="/projects/httpserver" date = "March 2022"></ProjectLink>
-        <ProjectLink title = "Solar sizing" link="/projects/solar" date = "August 2021"></ProjectLink>
-        <ProjectLink title = "Social media app" link="/projects/social" date = "August 2021"></ProjectLink>
+        <ProjectLink title = "Solar sizing" link="/projects/solar" date = "November 2021"></ProjectLink>
       </div>
       <div className = "other studio-section">
         <div className="other-title title">Other</div>
         <ProjectLink title = "OS resources/notes" link="/projects/os" date = "October 2022"></ProjectLink>
         <ProjectLink title = "Portfolio redesign" link="/projects/portfolio" date = "December 2021"></ProjectLink>
-        <ProjectLink title = "Battleship" link="/projects/battleship" date = "September 2021"></ProjectLink>
-        <ProjectLink title = "Weather app" link="/projects/weather" date = "October 2021"></ProjectLink>
+        <ProjectLink title = "Battleship" link="/projects/battleship" date = "November 2021"></ProjectLink>
+        <ProjectLink title = "Weather app" link="/projects/weather" date = "September 2021"></ProjectLink>
         {/* <ProjectLink title = "The other section" link="/projects/othersection" date = "September 2021"></ProjectLink> */}
         <ProjectLink title = "Website graveyard" link="/projects/webgraveyard" date = "August 2021"></ProjectLink>
       </div>
